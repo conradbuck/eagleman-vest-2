d27af7cb0698aaf3492da54ee6189217143f09f4
conradbuck <buckconrad0@gmail.com>
2025-04-23 19:30:48 -0700

    added two firmware samples

---
+++ firmware-rev1/ESP32-S3 Motor Driver Only/src/main.cpp
@@ -0,0 +1,89 @@
+#include <Wire.h>
+#include <Adafruit_DRV2605.h>
+#include <Arduino.h>
+#include <FastLED.h>
+
+#define DRVADDR 0x5A
+
+#define NUM_LEDS 1
+
+CRGB leds[NUM_LEDS];
+
+int effect = 0;
+
+Adafruit_DRV2605 drv;
+
+void setup() {
+  // put your setup code here, to run once:
+  FastLED.addLeds<WS2812B, 38, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(15);
+  leds[0] = CRGB::Black;
+  FastLED.show();
+  delay(250);
+
+  // if(!haptic.begin()) {
+  if(!drv.begin()) {
+    while(1) {
+      leds[0] = CRGB::Red;
+      FastLED.show();
+      delay(500);
+      leds[0] = CRGB::Black;
+      FastLED.show();
+      delay(500);
+      
+    }
+  } else {
+    for(int i = 0; i < 3; i++) {
+      leds[0] = CRGB::Green;
+      FastLED.show();
+      delay(250);
+      
+      delay(250);
+    }
+  }
+  drv.setMode(DRV2605_MODE_REALTIME);
+
+  delay(100);
+}
+
+uint8_t rtp_index = 0;
+uint8_t rtp[] = {
+  0x30, 100, 0x32, 100, 
+  0x34, 100, 0x36, 100, 
+  0x38, 100, 0x3A, 100,
+  0x00, 100,
+  0x40, 200, 0x00, 100, 
+  0x40, 200, 0x00, 100, 
+  0x40, 200, 0x00, 100
+};
+
+void loop() {
+  // // put your main code here, to run repeatedly:
+  // haptic.setIntensity(128);
+  // leds[0] = CRGB(10,255,100);
+  // FastLED.show();
+  // delay(200);
+  // leds[0] = CRGB(255,110,0);
+  // FastLED.show();
+  // delay(200);
+  // //haptic.setIntensity(0);
+  // leds[0] = CRGB(200,0,255);
+  // FastLED.show();
+  // delay(200);
+  // leds[0] = CRGB(255,255,255);
+  // FastLED.show();
+  // delay(200);
+
+  // delay(1000);
+  if (rtp_index < sizeof(rtp)/sizeof(rtp[0])) {
+    drv.setRealtimeValue(rtp[rtp_index]);
+    rtp_index++;
+    delay(rtp[rtp_index]);
+    rtp_index++;
+  } else {
+    drv.setRealtimeValue(0x00);
+    delay(1000);
+    rtp_index = 0;
+  }
+  
+}
